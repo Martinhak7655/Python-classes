8c71819bfe34e9fb3e2612d696bf02806dccd898
Martinhak7655 <martinhakobyan954@gmail.com>
2024-09-13 18:26:04 +0400

    Updates

---
+++ Tnayinner/Tnayin9/main.py
@@ -0,0 +1,14 @@
+import random
+
+def my_function():
+    new_array = []
+    for i in range(0, 10):
+        number = random.randint(1, 100)
+        for j in range(2, number):
+            if number % j == 0:
+                break
+            if j == number-1:
+                new_array.append(number)
+    print(new_array)
+my_function()
+
