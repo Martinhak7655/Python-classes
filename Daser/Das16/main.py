3e31c1dec154a683b029609e1b710d40222467b1
Martinhak7655 <martinhakobyan954@gmail.com>
2024-10-11 20:58:35 +0400

    Last Updates

---
+++ Daser/Das16/main.py
@@ -0,0 +1,47 @@
+# password = input("Enter Password")
+
+# user = [
+#     {
+#         "name": "Armen",
+#         "password": "1334"
+#     },
+#     {
+#         "name": "Garik",
+#         "password": "2564"
+#     }
+# ]
+
+# for i in user:
+#     if password == i["password"]:
+#         print(i["name"])
+
+# harcum = input("Cankanumeq mutq gorcel te grancvel >>  ")
+
+# user = [
+#     {
+#         "name": "Karen",
+#         "password": "3334"
+#     }
+# ]
+
+# if harcum == "mutq gorcel":
+#     password = input("Enter Password")
+#     for i in user:
+#         if password == i["password"]:
+#             print(f"Bari galust dzer ej {i["name"]}")
+#         else:
+#             print("Sxale Passwordy")
+            
+# elif harcum == "grancvel":
+#     name = input("Greq Dzer Anuny")
+#     password = input("Greq Dzer Passwordy")
+#     if len(password) <= 8:
+#         user2 = {
+#             "name": name,
+#             "password": password
+#         }
+#         user.append(user2)
+#         print("Shnrohavorumem duq hajoxutyamb grancveciq")
+#         print(user)
+#     else:
+#         print("Sxale Noric Pordzeq")
