2906a976e5a4c19d7dc83b4f32d813323f194f57
Martinhak7655 <martinhakobyan954@gmail.com>
2024-10-28 20:17:16 +0400

    First Database

---
+++ Daser/Das17/main.py
@@ -0,0 +1,25 @@
+import psycopg2 
+
+connection = psycopg2.connect(
+    host="localhost",
+    database="Shop",
+    user="postgres",
+    password="MH2012"
+)
+
+cursor = connection.cursor()
+create_table = '''
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+'''
+cursor.execute(create_table)
+connection.commit()
+insert = """
+INSERT INTO users (username, password) VALUES (%s, %s);
+"""
+cursor.execute(insert, ("Martin", "12345678"))
+connection.commit()
