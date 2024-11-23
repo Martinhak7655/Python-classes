99fbf001b92fbd64b4ec3c6a15884b5b46b2ea22
Martinhak7655 <martinhakobyan954@gmail.com>
2024-11-23 18:18:59 +0400

    Last Update

--- Daser/Das22/main.py
+++ Daser/Das22/main.py
@@ -1,17 +1,31 @@
-# import telebot
+import telebot
+import random
 
-# BOT_TOKEN = ""
+BOT_TOKEN = "7912861654:AAEKPUAzwLx3zf9AUI9l8zS4rhzVNQxaAQc"
 
-# bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
-# @bot.message_handler(commands=["start"])
-# def start(message):
-#     user_name = message.from_user.first_name
-#     bot.reply_to(message, f"Hello {user_name}, Im bot. Get my id /myid")
+sticker_answer = ["Good Sticker!", "Perfeckt", "Good"]
+image_aswer = ["Good image!", "Beautiful image!", "good"]
 
-# @bot.message_handler(commands=["myid"])
-# def my_id(message):
-#     user_id = message.from_user.id
-#     bot.reply_to(message, f"<b><i>Your Id ➡️ {user_id}</i></b>", parse_mode="HTML")
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_name = message.from_user.first_name
+    bot.reply_to(message, f"Hello {user_name}, Im bot. Get my id /myid")
 
-# bot.polling()
+@bot.message_handler(commands=["myid"])
+def my_id(message):
+    user_id = message.from_user.id
+    bot.reply_to(message, f"<b><i>Your Id ➡️ {user_id}</i></b>", parse_mode="HTML")
+
+@bot.message_handler(content_types=["sticker"])
+def sticker(message):
+    sticker_random = random.randint(0, len(sticker_answer) - 1)
+    bot.reply_to(message, sticker_answer[sticker_random])
+
+@bot.message_handler(content_types=["photo"])
+def image(message):
+    image_random = random.randint(0, len(image_aswer) - 1)
+    bot.reply_to(message, image_aswer[image_random])
+
+bot.polling()
---
+++ Daser/Das23/main.py
@@ -0,0 +1,48 @@
+import telebot
+import psycopg2
+from telebot import types
+
+BOT_TOKEN = "7404397306:AAEsXz6RaTETQGKE7epm-tQr7SwjWSKx4r8"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+connection = psycopg2.connect(
+    host="localhost",
+    database='signinbot',
+    user="postgres",
+    password="MH2012"
+)
+cursor = connection.cursor()
+
+create_table = '''
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+'''
+cursor.execute(create_table)
+connection.commit()
+
+@bot.message_handler(commands=["start"])
+def signin(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    btn1 = types.KeyboardButton("/signin")
+    markup.add(btn1)
+    bot.reply_to(message, "Hi! I'm Bot", reply_markup=markup)
+
+@bot.message_handler(commands=["signin"])
+def signin(message):
+    bot.reply_to(message, "ok lets type your username and password")
+
+@bot.message_handler(content_types=["text"])
+def types2(message):
+    username = message.text
+    password = message.text
+    insert = '''
+        INSERT INTO users (username, password) VALUES (%s, %s);
+    '''
+    cursor.execute(insert, (username, password,))
+    connection.commit()
+
+bot.polling()
