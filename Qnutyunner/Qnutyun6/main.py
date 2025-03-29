17f45b4e6816acadf9e09a8f3d5116e02e705485
Martinhak7655 <martinhakobyan954@gmail.com>
2025-03-29 19:20:43 +0400

    Last Update

---
+++ Qnutyunner/Qnutyun6/main.py
@@ -0,0 +1,65 @@
+import turtle
+
+t = turtle.Turtle()
+
+# t.pencolor("blue")
+# t.forward(150)
+
+# t.pencolor("red")
+# t.forward(150)
+
+# t.pencolor("green")
+# t.forward(150)
+
+# color = ["blue", "green", "yellow"]
+# for i in range(3):
+#     t.pencolor(color[i])
+#     t.forward(200)
+#     t.left(120)
+
+# color = ["blue", "green", "yellow", "orange"]
+# for i in range(4):
+#     t.pencolor(color[i])
+#     t.forward(200)
+#     t.left(90)
+
+# for i in range(4):
+#     t.forward(300)
+#     t.right(90)
+
+# t.forward(300 // 2)
+# t.right(90)
+# t.forward(300)
+# t.right(90)
+# t.forward(300 // 2)
+# t.right(90)
+# t.forward(150)
+
+# t.right(90)
+# t.forward(300)
+
+# for i in range(4): 
+#     t.forward(300)
+#     t.right(90)
+
+# t.forward(100)
+# t.right(90)
+# t.forward(300)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(300)
+# t.right(90)
+# t.forward(100)
+
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(300)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(300)
+
+
+turtle.done()
